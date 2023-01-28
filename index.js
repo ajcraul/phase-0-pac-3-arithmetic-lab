ff1cd25b404c15a9d46d1c6605bb1baa485f6f8a
allison craul <craul.aj@gmail.com>
2023-01-28 14:50:23 -0700

    completed assignment

---
+++ index.js
@@ -0,0 +1,31 @@
+function add(a, b) {
+    return a + b;
+}
+
+function subtract(c, d) {
+    return c - d;
+}
+
+function divide(e, f) {
+    return e / f;
+}
+
+function multiply(g, h) {
+    return g * h;
+}
+
+function increment(n) {
+    return (n += 1);
+}
+
+function decrement(n) {
+    return (n -= 1);
+}
+
+function makeInt(n) {
+   return parseInt(n, 10);
+}
+
+function preserveDecimal(n) {
+    return parseFloat(n);
+}
